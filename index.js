d1070e5222bfe9d073e5b131dabc900b36ac0ff4
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-22 18:25:21 +0300

    add function

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const colors = [
 ];
 
 const startBtn = document.querySelector('button[data-action="start"]') 
-console.log(startBtn)
+// console.log(startBtn)
 const stopBtn = document.querySelector('button[data-action="stop"]')
-console.log(stopBtn)
+// console.log(stopBtn)
+
+startBtn.addEventListener('click', onBtnStartClick)
+stopBtn.addEventListener('click', onBtnStopClick)
+
+function onBtnStartClick() { 
+  console.log('click on start')
+}
+
+function onBtnStopClick() {
+  console.log('click on stop')
+}
+
+function getItem() {
+   for (let i = 0; i < colors.length; i += 1) {
+    console.log(colors)
+  }
+}
+ 
+const randomIntegerFromInterval = (min, max) => {
+return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
