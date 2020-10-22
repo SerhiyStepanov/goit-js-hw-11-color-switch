6cda593d2c38ba607e2fc9f948e8cace6a570d38
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-22 20:08:47 +0300

    work

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const colors = [
   '#009688',
   '#795548',
 ];
+console.log(colors)
 
 const startBtn = document.querySelector('button[data-action="start"]') 
 // console.log(startBtn)
@@ -23,13 +24,8 @@ function onBtnStopClick() {
   console.log('click on stop')
 }
 
-function getItem() {
-   for (let i = 0; i < colors.length; i += 1) {
-    console.log(colors)
-  }
-}
- 
-const randomIntegerFromInterval = (min, max) => {
-return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
+ 
+// const randomIntegerFromInterval = (min, max) => {
+// return Math.floor(Math.random() * (max - min + 1) + min);
+// };
